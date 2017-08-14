c2749e0f2510765cd30345cfc51ec8fb67c41d1e
Daniel <mytechde@outlook.com>
2017-08-14 17:52:15 +0200

    added bootstrap

---
+++ src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { FooterComponent } from './components/layout/footer/footer.component';
+import { NavigationComponent } from './components/layout/navigation/navigation.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavigationComponent,
+    FooterComponent,
+    HomeComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
